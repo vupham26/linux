3bdc3843d14162108ac610bd76f1fdedb58311b7
Lukas Wunner <lukas@wunner.de>
2015-12-16 21:13:39 +0100

    drm/radeon: Defer probe if gmux is present but its driver isn't
    gmux is a microcontroller built into dual GPU MacBook Pros.
    On pre-retina MBPs, if we're the inactive GPU, we need apple-gmux
    to temporarily switch DDC so that we can probe the panel's EDID.
    
    Note that if gmux is present but the kernel was configured without
    VGA_ARB or VGA_SWITCHEROO, the driver will never load. Log an
    informational message to hopefully reduce support requests to a
    minimum.
    
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=61115
    Signed-off-by: Lukas Wunner <lukas@wunner.de>

--- drivers/gpu/drm/radeon/radeon_drv.c
+++ drivers/gpu/drm/radeon/radeon_drv.c
@@ -34,9 +34,11 @@
 #include "radeon_drv.h"
 
 #include <drm/drm_pciids.h>
+#include <linux/apple-gmux.h>
 #include <linux/console.h>
 #include <linux/module.h>
 #include <linux/pm_runtime.h>
+#include <linux/vgaarb.h>
 #include <linux/vga_switcheroo.h>
 #include <drm/drm_gem.h>
 
@@ -318,6 +320,16 @@ static int radeon_pci_probe(struct pci_dev *pdev,
 			    const struct pci_device_id *ent)
 {
 	int ret;
+
+	/*
+	 * apple-gmux is needed on dual GPU MacBook Pro
+	 * to probe the panel if we're the inactive GPU.
+	 */
+	if (apple_gmux_present() && pdev != vga_default_device() &&
+	    !vga_switcheroo_handler_flags()) {
+		DRM_INFO("Probe deferred until apple-gmux driver is ready\n");
+		return -EPROBE_DEFER;
+	}
 
 	/* Get rid of things like offb */
 	ret = radeon_kick_out_firmware_fb(pdev);
